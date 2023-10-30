3bf221817671740b5d88a0f72d9409915105419a
noofny <noofny@gmail.com>
2023-10-30 12:02:01 +1100

    Upgrades Ubuntu

--- setup.sh
+++ setup.sh
@@ -62,7 +62,8 @@ export DOMAIN_NAME=${DOMAIN_NAME}
 #       Path for 'remote' storage will be '/mnt/proxmox/template/cache/'
 # TODO: make this dynamic so the user can choose!
 CONTAINER_OS_TYPE='ubuntu'
-CONTAINER_OS_VERSION='ubuntu-22.04-standard_22.04-1_amd64.tar.zst'
+# CONTAINER_OS_VERSION='ubuntu-22.04-standard_22.04-1_amd64.tar.zst'
+CONTAINER_OS_VERSION='ubuntu-23.04-standard_23.04-1_amd64.tar.zst'
 TEMPLATE_LOCATION="remote:vztmpl/${CONTAINER_OS_VERSION}"
 info "Using template: ${TEMPLATE_LOCATION}"
 
