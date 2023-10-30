4d07add87960ddccc7f33ec53c16704bd638caa4
noofny <noofny@gmail.com>
2023-10-30 12:54:48 +1100

    cleanup

--- setup.sh
+++ setup.sh
@@ -58,7 +58,6 @@ export HOST_IP4_CIDR=${HOST_IP4_CIDR}
 #       Path for 'remote' storage will be '/mnt/proxmox/template/cache/'
 # TODO: make this dynamic so the user can choose!
 CONTAINER_OS_TYPE='ubuntu'
-# CONTAINER_OS_VERSION='ubuntu-22.04-standard_22.04-1_amd64.tar.zst'
 CONTAINER_OS_VERSION='ubuntu-23.04-standard_23.04-1_amd64.tar.zst'
 TEMPLATE_LOCATION="remote:vztmpl/${CONTAINER_OS_VERSION}"
 info "Using template: ${TEMPLATE_LOCATION}"
